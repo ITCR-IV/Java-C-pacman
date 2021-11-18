8916e05128815a320e062f0c178c6981bb87dfa3
Ignacio Vargas <ignaevc@gmail.com>
2021-11-18 11:50:00 -0600

    Finished setting up socket manager with connection to server and method to send jsons

--- PacMan/src/Main.java
+++ PacMan/src/Main.java
@@ -1,14 +1,25 @@
 import javax.swing.*;
 
+import com.github.cliftonlabs.json_simple.JsonObject;
+
 public class Main {
 
     public static void main(String[] args) {
         Window pacManWindow = new Window();
-        pacManWindow.setVisible(true);
-        pacManWindow.setTitle("Pac-Man");
-        pacManWindow.setSize(635,775);
-        pacManWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         pacManWindow.setLocationRelativeTo(null);
+        pacManWindow.setVisible(true);
+        pacManWindow.setTitle("Pa c-Man");
+        pacManWindow.setSize(635, 775);
+        pacManWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        SockManager manager = new SockManager("localhost", 8888);
+
+        Thread listenerThread = new Thread(manager);
+        listenerThread.start();
+        try {
+            listenerThread.join();
+        } catch (InterruptedException i) {
+            System.out.println(i);
+        }
 
     }
 }
--- PacMan/src/SockManager.java
+++ PacMan/src/SockManager.java
@@ -1,16 +1,39 @@
 import java.net.*;
+import java.util.Set;
 import java.io.*;
-import org.json.simple.JSONObject;
+import com.github.cliftonlabs.json_simple.*;
 
-public class SockManager {
+public class SockManager implements Runnable {
     public enum ClientType {
         OBSERVER, PLAYER
     }
 
+    // private enum PossibleKeys implements JsonKey {
+    // ERROR("Error");
+
+    // private final Object value;
+
+    // PossibleKeys(final Object value) {
+    // this.value = value;
+    // }
+
+    // @Override
+    // public String getKey() {
+    // return this.name();
+    // }
+
+    // @Override
+    // public Object getValue() {
+    // return this.value;
+    // }
+    // }
+
     // initialize socket output stream
     private Socket socket = null;
-    private DataOutputStream out = null;
+    private PrintWriter out = null;
+    private BufferedReader in = null;
     private ClientType type;
+    private boolean quit = false;
 
     private void buildSocket(String address, int port) {
         // establish a connection
@@ -18,8 +41,8 @@ public class SockManager {
             socket = new Socket(address, port);
             System.out.println("Connected");
 
-            // sends output to the socket
-            out = new DataOutputStream(socket.getOutputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(new BufferedInputStream(socket.getInputStream())));
         } catch (UnknownHostException u) {
             System.out.println(u);
         } catch (IOException i) {
@@ -31,13 +54,9 @@ public class SockManager {
     public SockManager(String address, int port) {
         type = ClientType.PLAYER;
         buildSocket(address, port);
-        try {
-            JSONObject json = new JSONObject();
-            json.put("init", "player");
-            out.writeUTF(json.toString());
-        } catch (IOException i) {
-            System.out.println(i);
-        }
+        JsonObject json = new JsonObject();
+        json.put("init", "player");
+        out.println(Jsoner.serialize(json));
     }
 
     // constructor to put ip address and port
@@ -45,24 +64,51 @@ public class SockManager {
         type = ClientType.OBSERVER;
         buildSocket(address, port);
 
-        try {
-            JSONObject json = new JSONObject();
-            json.put("init", "player");
-            json.put("observee", observedPlayer);
-            out.writeUTF(json.toString());
-        } catch (IOException i) {
-            System.out.println(i);
-        }
+        JsonObject json = new JsonObject();
+        json.put("init", "observer");
+        json.put("observee", observedPlayer);
+        out.println(Jsoner.serialize(json));
 
+    }
+
+    public void sendJSON(JsonObject json) {
+        out.println(Jsoner.serialize(json));
     }
 
     // cerrar conexión
     public void closeSocket() {
-        try {
-            out.close();
-            socket.close();
-        } catch (IOException i) {
-            System.out.println(i);
+        synchronized (socket) {
+            quit = true;
+            if (socket.isClosed()) {
+                return;
+            }
+            try {
+                out.close();
+                socket.close();
+            } catch (IOException i) {
+                System.out.println(i);
+            }
         }
+    }
+
+    public void run() {
+        while (!quit) {
+            try {
+                JsonObject json = (JsonObject) Jsoner.deserialize(in);
+                Set<String> keys = json.keySet();
+
+                if (keys.contains("Error")) {
+                    final JsonKey errorKey = Jsoner.mintJsonKey("Error", "Wrongful socket operation with server.");
+                    quit = true;
+                    System.out.println("Error en socket: " + json.getString(errorKey));
+                    closeSocket();
+                }
+
+            } catch (JsonException j) {
+                System.out.println(j);
+                quit = true;
+            }
+        }
+        System.out.println("Closing connection");
     }
 }
